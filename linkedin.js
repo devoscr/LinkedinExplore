d25e5fe21a673e6a52a0d020b722ac94ad8c5f38
Oscar <Boutarfa@MacBook-Pro-de-Sabah.local>
2023-06-06 23:10:44 +0200

    [fixed env privacy]

--- linkedin.js
+++ linkedin.js
@@ -1,20 +1,19 @@
 const { chromium } = require('playwright');
+require('dotenv').config();
 
-(async () => {
+async function  toJobOffersPage()  {
     const browser = await chromium.launch( {headless: false} );
     const page = await browser.newPage();
     await page.goto('https://www.linkedin.com');
     await page.waitForTimeout(5000);
-    await page.fill('#session_key', 'oscar.boutarfa@proton.me');
-    await page.fill('#session_password', 'oscoBtf75018!');
+    await page.fill('#session_key', process.env.LKDN_USRNM);
+    await page.fill('#session_password', process.env.LKDN_PSSWD);
     await page.click('[data-id="sign-in-form__submit-btn"]');
     await page.waitForNavigation();
     const linkOffersJob = await page.$eval('ul.global-nav__primary-items > li:nth-child(3) > a', (element) => element.href);
     await page.goto(linkOffersJob);
     await page.waitForNavigation();
-    const jobOffers =  await page.$eval('ul.jobs-home-vertical-list__entity-list > li:nth-child(2) > search > ember1044 > ember1047 > ember1048', (element) => element)
-    console.log(jobOffers);
     await browser.close();
+}
 
-
-})();
+toJobOffersPage();
